c10bf559c6fe946d34f8443ea0299f68c3eb5bf2
Akasiek <kpomykala2002@gmail.com>
2023-02-26 20:49:16 +0100

    feat: new font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Poppins, sans-serif", { fontFeatureSettings: '"cv11", "ss01"' }],
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
